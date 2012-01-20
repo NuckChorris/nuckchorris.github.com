2ccab21d4846b2f18fd4db54696d997f4c6f8477
Peter Lejeck <peter.lejeck@gmail.com>
2012-01-19 20:03:45 -0800

    Adding cwVer file for Chromacity
    Getting ready to test this script

---
+++ cw_vers/chromacity.js
@@ -0,0 +1,9 @@
+cwVer.callbacks.1e7c1c9bc9e70174393f7503b8276fc4({
+	'name': 'Chromacity',
+	'version': '2.0.0',
+	'downloads': {
+		'chrome': 'http://github.com/nuckchorris/chromacity',
+		'firefox': 'https://github.com/nuckchorris/chromacity',
+		'safari': 'https://github.com/nuckchorris/chromacity'
+	}
+});
